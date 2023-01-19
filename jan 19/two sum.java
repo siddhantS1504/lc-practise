ee7bcd0179ab31775152816cfafb914c3c8b4330
siddhantS1504 <siddhantsaxena1504@gmail.com>
2023-01-20 00:01:29 +0530

    Update two sum.java

--- jan 19/two sum.java
+++ jan 19/two sum.java
@@ -3,27 +3,22 @@ question link :https://leetcode.com/problems/container-with-most-water/descripti
 
 class Solution {
     public int maxArea(int[] height) {
-        int c=0;
-        int area=0;
-        for(int i=0;i<height.length-1;i++)
-        {
-            
-            
-            for(int j=i+1;j<height.length;j++)
-            {
-                
-                
-                if(area<Math.min(height[i],height[j])*(j-i))
-            {
-                area=Math.min(height[i],height[j])*(j-i);
-            }
-                
-            }
-             
-            
-
-        }
-        return area;
         
+        int area=0;
+        int l = 0;
+        int r = height.length - 1;
+        int max = 0;
+        while(l < r){
+            int w = r - l;
+            int h = Math.min(height[l], height[r]);
+            area = h * w;
+            max = Math.max(max, area);
+            if(height[l] < height[r]) l++;
+            else if(height[l] > height[r]) r--;
+            else {
+                l++;
+                r--;
+            }
+        }
+        return max;
     }
-}
