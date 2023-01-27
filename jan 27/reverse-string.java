9d5da2b60501d004fda3abfb26943cbad7a7bec5
siddhantS1504 <siddhantsaxena1504@gmail.com>
2023-01-27 23:11:30 +0530

    Create reverse-string.java

---
+++ jan 27/reverse-string.java
@@ -0,0 +1,14 @@
+QUESTION LINK: https://leetcode.com/problems/reverse-string/description/
+
+class Solution {
+    public void reverseString(char[] s) {
+        int low = 0, high = s.length-1;
+        while(low<=high){
+            char temp = s[low];
+            s[low] = s[high];
+            s[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}
