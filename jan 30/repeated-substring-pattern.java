1c92b5cb3881d621670a4914e5058e987e64b31c
siddhantS1504 <siddhantsaxena1504@gmail.com>
2023-01-30 23:44:58 +0530

    Create repeated-substring-pattern.java

---
+++ jan 30/repeated-substring-pattern.java
@@ -0,0 +1,13 @@
+QUESTION LINK: https://leetcode.com/problems/repeated-substring-pattern/description/
+
+
+class Solution { 
+	public boolean repeatedSubstringPattern(String s) {
+		String str=s+s;
+		String str1=str.substring(1, str.length()-1); 
+		if(str1.contains(s)) 				
+			return true;
+		else
+		   return false;
+	}
+}
