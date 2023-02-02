ebc55c1a3d63de286e4e7d9443550f3c254af54a
siddhantS1504 <siddhantsaxena1504@gmail.com>
2023-02-02 23:35:33 +0530

    Create Shuffle-String.java

---
+++ feb 2/Shuffle-String.java
@@ -0,0 +1,11 @@
+QUESTION LINK: https://leetcode.com/problems/shuffle-string/description/
+
+
+class Solution {
+    public String restoreString(String s, int[] in) {
+        char[] c = new char[in.length];
+        for(int i = 0; i < in.length; i++)
+            c[in[i]] = s.charAt(i);
+        return new String(c);
+    }
+}
