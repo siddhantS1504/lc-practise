fa2081c05e0da40719b0c0d37f12615f6cbd4607
siddhantS1504 <siddhantsaxena1504@gmail.com>
2023-02-28 23:39:24 +0530

    Create jump-game.java

---
+++ feb-28/jump-game.java
@@ -0,0 +1,25 @@
+PROBLEM LINK: https://leetcode.com/problems/jump-game/
+
+class Solution {
+
+    public boolean canJump(int[] nums) {
+
+     int boundary = 0;
+
+     for(int i =0;i<=boundary;i++){
+
+         boundary = Math.max(boundary,i+nums[i]);
+
+         if(boundary >=nums.length-1)
+
+         return true;
+
+     } 
+
+     return false;
+
+    }
+
+}
+
+
