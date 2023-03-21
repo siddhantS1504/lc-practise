19192b53966f2ac7e4bb335c29e17b584d5b3fb0
siddhantS1504 <siddhantsaxena1504@gmail.com>
2023-03-21 22:53:31 +0530

    Create minimum-moves-to-convert-string.java

---
+++ mar-21/minimum-moves-to-convert-string.java
@@ -0,0 +1,18 @@
+PROBLEM LINK: https://leetcode.com/problems/minimum-moves-to-convert-string/description/
+
+class Solution {
+    public int minimumMoves(String s) {
+        int i=0;
+        int step=0;
+        while(i<s.length()){
+            if(s.charAt(i)=='X'){
+                i=i+3;
+                step++;
+            }
+            else{
+                i++;
+            }
+        }
+        return step;
+    }
+}
